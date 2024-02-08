502c73f74dcfdeb7ae0d208c810026d1337726fb
luiz.carmo <luiz.carmo@animaeducacao.com.br>
2024-02-08 18:26:12 -0300

    Navigation drawer colocado na aplicação

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <!-- <v-navigation-drawer v-model="drawer"> -->
+    <!--  -->
+    <!-- </v-navigation-drawer> -->
+
+    <v-navigation-drawer 
+      v-model="drawer"
+      temporary=""
+    >
+      <v-list>
+        <v-list-item title="Lista de Tarefas" subtitle="Vue.js e Vuetify"></v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
+        <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+
     <v-main>
-      <router-view/>
+      <!--  -->
     </v-main>
   </v-app>
 </template>
 
+<script setup>
+import { ref } from 'vue'
+
+const drawer = ref(null)
+</script>
+
 <script>
-
 export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
+  data: () => ({ drawer: null }),
 }
 </script>
