b8fe7cb67873d54ecb2053655f65a234afd4f2e0
luiz.carmo <luiz.carmo@animaeducacao.com.br>
2024-03-13 17:27:31 -0300

    app-bar, navigation-drawer e main iniciados

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <v-app id="inspire">
 
-    <v-navigation-drawer 
-      v-model="drawer"
-      temporary
-    >
+    <v-navigation-drawer v-model="drawer" temporary>
       <v-list>
         <v-list-item title="Lista de Tarefas" subtitle="Vue.js e Vuetify"></v-list-item>
       </v-list>
@@ -12,25 +9,36 @@
       <v-divider></v-divider>
 
       <v-list density="compact" nav>
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          :prepend-icon="item.icon"
+        <v-list-item v-for="item in items" 
+          :to="item.to" 
+          :key="item.title" 
+          :prepend-icon="item.icon" 
           :title="item.title"
-          :value="item.value"
-        >
+          :value="item.value">
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-app-bar
+      color="teal-darken-4"
+      :image="require('./assets/calendar.jpg')"
+      density="prominent"
+    >
+      <template v-slot:image>
+        <v-img
+          gradient="to top right, rgba(19,84,122,.5), rgba(28,108,199,.8)"
+        ></v-img>
+      </template>
 
-      <v-app-bar-title>Application</v-app-bar-title>
-    </v-app-bar>
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon location="left" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      </template>
+
+      <v-app-bar-title></v-app-bar-title>
+    </v-app-bar>    
 
     <v-main>
-      <!--  -->
+      <router-view/>
     </v-main>
   </v-app>
 </template>
@@ -50,11 +58,13 @@ export default {
         title: 'Tarefas',
         value: 'home',
         icon: 'mdi-check-circle',
+        to: '/'
       },
       {
         title: 'Sobre',
         value: 'about',
         icon: 'mdi-information',
+        to: '/about'
       },
     ],
   }),
